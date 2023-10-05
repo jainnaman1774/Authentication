eda6fe4b2ad72738213eb6bcd6cfd0a0f647c59a
Prachigetgit <126250683+Prachigetgit@users.noreply.github.com>
2023-10-05 15:26:06 +0530

    first commit

---
+++ client/src/Home.jsx
@@ -0,0 +1,15 @@
+
+
+
+function Home(){
+    return(
+        <>
+                     <h1 style={{textAlign: 'center'}}>Login Successfully</h1>
+          <h2 style={{textAlign: 'center'}}>Welcome home</h2>
+        </>
+
+    )
+}
+
+
+export default Home;
